5c8b61a60e139499d7498a017d93e46618326a3a
aaroncaic <chenga2@rpi.edu>
2016-09-29 03:13:45 -0400

    Added some code to allow user to drag and move the circle around.

--- CCApp/CCApp.java
+++ CCApp/CCApp.java
@@ -13,7 +13,7 @@ import java.applet.*;
 import java.net.URL;
 import javax.imageio.ImageIO;
 
-public class CCApp extends Applet implements Runnable
+public class CCApp extends Applet implements Runnable, MouseListener, MouseMotionListener
 {
   Thread runner;
   Image Buffer;
@@ -23,7 +23,11 @@ public class CCApp extends Applet implements Runnable
   Image football;
   double ONE_YARD;
   int FIFTY_YARD_LINE;
-  
+
+  int x, y;    // the coordinates of the circle/dot/player
+  int mx, my;  // the most recently recorded mouse coordinates
+  boolean isMouseDraggingBox = false;
+
   public void init()
   {
     width=this.getSize().width;
@@ -34,13 +38,18 @@ public class CCApp extends Applet implements Runnable
     football = getImage(getCodeBase(), "football2.jpg");
     ONE_YARD = 12.25;
     FIFTY_YARD_LINE = 636;
+
+    x = 600;
+    y = 300;
+    addMouseListener(this);
+    addMouseMotionListener(this);
   }
 
   private class MyKeyListener extends KeyAdapter
   {
     public void keyPressed(KeyEvent e)
     {
-      switch(e.getKeyCode())
+      switch (e.getKeyCode())
       {
         case KeyEvent.VK_A:
         leftKey = true;
@@ -71,7 +80,7 @@ public class CCApp extends Applet implements Runnable
 
     public void keyReleased(KeyEvent e)
     {
-      switch(e.getKeyCode())
+      switch (e.getKeyCode())
       {
         case KeyEvent.VK_A:
         leftKey = false;
@@ -124,15 +133,61 @@ public class CCApp extends Applet implements Runnable
     return true;
   }
 
+  public void mouseEntered( MouseEvent e ) {}
+
+  public void mouseExited( MouseEvent e ) {}
+
+  public void mouseClicked( MouseEvent e ) {}
+
+  public void mousePressed( MouseEvent e )
+  {
+    mx = e.getX();
+    my = e.getY();
+    if (x < mx && mx < x+30 && y < my && my < y+30)
+    {
+      isMouseDraggingBox = true;
+    }
+    e.consume();
+  }
+
+  public void mouseReleased(MouseEvent e)
+  {
+    isMouseDraggingBox = false;
+    e.consume();
+  }
+
+  public void mouseMoved(MouseEvent e) {}
+
+  public void mouseDragged(MouseEvent e)
+  {
+    if (isMouseDraggingBox)
+    {
+      // get the latest mouse position
+      int new_mx = e.getX();
+      int new_my = e.getY();
+
+      // displace the box by the distance the mouse moved since the last event
+      x += new_mx - mx;
+      y += new_my - my;
+
+      // update our data
+      mx = new_mx;
+      my = new_my;
+
+      repaint();
+      e.consume();
+    }
+  }
+
   public void run()
   {
     while(true)
     {
       try {runner.sleep(13);}
-      catch (Exception e) { }
+      catch (Exception e) {}
       gBuffer.drawImage(football, 0, 0, this);
       gBuffer.setColor(Color.blue);
-      gBuffer.fillOval(600,300,30,30);
+      gBuffer.fillOval(x,y,30,30);
       gBuffer.drawLine(FIFTY_YARD_LINE,0,FIFTY_YARD_LINE,700);
 
       gBuffer.setColor(Color.red);
