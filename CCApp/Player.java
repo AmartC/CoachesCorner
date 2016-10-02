658be1885c6635c5b96cdb2b36b8de2a06f4461c
aaroncaic <chenga2@rpi.edu>
2016-10-02 14:50:18 -0400

    Added a Player class.

---
+++ CCApp/Player.java
@@ -0,0 +1,23 @@
+/**Authors:
+ * Timothy Castiglia
+ * Amartya Chakraborty
+ * Ethan Fox
+ * Aaron Cheng
+ * Date: 10/2/2016
+ */
+
+public class Player
+{
+  int x;
+  int y;
+
+  public int getX()
+  {
+    return x;
+  }
+
+  public int getY()
+  {
+    return y;
+  }
+}
